3b3935076bbb5e9719216a2774d8842f670dfb32
chanasit-sae <chanasit.sae@kkumail.com>
2024-12-23 21:04:27 +0700

    lab5

---
+++ docs/lab5/NumberAnalyzer/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"saetkong.chanasit.lab5"}];updateSearchResults();
---
+++ docs/lab5/NumberAnalyzer/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"saetkong.chanasit.lab5","l":"NumberAnalyzer"}];updateSearchResults();
